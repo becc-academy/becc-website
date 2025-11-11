237f2c45c4ada231254dc0eb956de7e864397f7b
Grandkojo <essienernest.kojoowusu@gmail.com>
2025-11-11 18:24:18 +0000

    feat(website): setup react-router and create placeholder tsx for required routes

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import NotFoundPage from '@/pages/404Page';
+import AboutPage from '@/pages/AboutPage';
+import ContactPage from '@/pages/ContactPage';
+import EventsPage from '@/pages/EventsPage';
 import HomePage from '@/pages/HomePage';
+import ProgramsPage from '@/pages/ProgramsPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/programs" element={<ProgramsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
